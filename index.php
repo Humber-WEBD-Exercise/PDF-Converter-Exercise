94f58fc7ed1fb2a3232feb4bbef0f35649a2f228
joannjiang <qiongrongjiang@gmail.com>
2016-11-21 13:01:31 -0500

    style receipt

--- index.php
+++ index.php
@@ -7,50 +7,102 @@
   <head>
     <meta charset="utf-8">
     <title>invoice #<?php echo $invoice['id'] ?></title>
-    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <style>
+      *{
+        padding: 0;
+        margin: 0;
+      }
+      .invoice-box{
+        max-width:800px;
+        margin: 20px auto;
+        padding:30px;
+        font-size:16px;
+        line-height:24px;
+        font-family:'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        color:#555;
+      }
+
+      .invoice-box table{
+        width:100%;
+        text-align:left;
+      }
+
+      .invoice-box table td{
+        padding:5px;
+        vertical-align:top;
+      }
+
+      .invoice-box table tr td:nth-child(2), .invoice-box table tr td:nth-child(3), .invoice-box table tr td:nth-child(4){
+        text-align:right;
+      }
+
+      .invoice-box table tr.top table td{
+        padding-bottom:20px;
+      }
+
+      .invoice-box .title{
+        font-size:45px;
+        line-height:45px;
+        color:#333;
+      }
+
+      .invoice-box table tr.information td{
+        padding:40px 0;
+      }
+
+      .invoice-box table tr.heading td{
+        background:#eee;
+        border-bottom:1px solid #ddd;
+        font-weight:bold;
+      }
+
+      .invoice-box table tr.item td{
+        border-bottom:1px solid #eee;
+      }
+
+      .invoice-box table tr.item:last-of-type td{
+        border-bottom:none;
+      }
+
+      .invoice-box table tr.total.first td:nth-child(2){
+        padding-top: 20px;
+        border-top:2px solid #eee;
+      }
+      .invoice-box table tr.total.last td:nth-child(2){
+        font-weight:bold;
+        padding-bottom: 40px;
+      }
+    </style>
   </head>
   <body>
     <div class="invoice-box">
         <table cellpadding="0" cellspacing="0">
             <tr class="top">
+                <td class="title" colspan="2">
+                    Humber College
+                </td>
                 <td colspan="2">
-                    <table>
-                        <tr>
-                            <td class="title">
-                                Humber College
-                            </td>
-
-                            <td>
-                                Invoice #: <?php echo $invoice['id'] ?><br>
-                                Status: <?php echo $invoice['status'] ?><br>
-                                Date: <?php echo $invoice['date'] ?>
-                            </td>
-                        </tr>
-                    </table>
+                    Invoice #: <?php echo $invoice['id'] ?><br>
+                    Status: <?php echo $invoice['status'] ?><br>
+                    Date: <?php echo $invoice['date'] ?>
                 </td>
             </tr>
 
             <tr class="information">
-                <td colspan="2">
-                    <table>
-                        <tr>
-                            <td>
-                                <?php echo$invoice['address']['street']; ?>, <?php echo$invoice['address']['postal']; ?><br>
-                                <?php echo$invoice['address']['province']; ?>, <?php echo$invoice['address']['country']; ?>
-                            </td>
+              <td colspan="2">
+                  <?php echo$invoice['address']['street']; ?>, <?php echo$invoice['address']['postal']; ?><br>
+                  <?php echo$invoice['address']['province']; ?>, <?php echo$invoice['address']['country']; ?>
+              </td>
 
-                            <td>
-                                <?php echo$invoice['contact']['last']; ?>, <?php echo$invoice['contact']['first']; ?><br>
-                                <?php echo$invoice['contact']['email']; ?>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
+              <td colspan="2">
+                  <?php echo$invoice['contact']['last']; ?>, <?php echo$invoice['contact']['first']; ?><br>
+                  <?php echo$invoice['contact']['email']; ?>
+              </td>
             </tr>
 
             <tr class="heading">
                 <td>
-                    Product
+                  Product
                 </td>
                 <td>
                   Quantity
@@ -80,43 +132,46 @@
            <?php endforeach; ?>
 
             <tr class="total first">
-               <td></td>
+              <td colspan="2">
 
-               <td>
-                  Subtotal: $<?php echo number_format($invoice['subtotal'],2); ?>
-               </td>
+              </td>
+              <td colspan="2">
+                Subtotal: $<?php echo number_format($invoice['subtotal'],2); ?>
+              </td>
             </tr>
 
             <tr class="total">
-               <td></td>
+              <td colspan="2">
 
-               <td>
-                  Shipping: $<?php echo number_format($invoice['shipping'],2); ?>
-               </td>
+              </td>
+             <td colspan="2">
+                Shipping: $<?php echo number_format($invoice['shipping'],2); ?>
+             </td>
             </tr>
 
             <tr class="total">
-               <td></td>
+              <td colspan="2">
 
-               <td>
-                  Tax: $<?php echo number_format($invoice['tax'],2); ?>
-               </td>
+              </td>
+             <td colspan="2">
+                Tax: $<?php echo number_format($invoice['tax'],2); ?>
+             </td>
             </tr>
 
             <tr class="total last">
-                <td></td>
+              <td colspan="2">
 
-                <td>
-                   Total: $<?php echo number_format($invoice['total'],2); ?>
-                </td>
+              </td>
+              <td colspan="2">
+                 Total: $<?php echo number_format($invoice['total'],2); ?>
+              </td>
             </tr>
         </table>
-    </div>
 
-    <div>
-      <b>Template thanks to : </b><a href="https://github.com/NextStepWebs/simple-html-invoice-template">https://github.com/NextStepWebs/simple-html-invoice-template</a>
+        <p>
+          Template thanks to : <a href="https://github.com/NextStepWebs/simple-html-invoice-template">https://github.com/NextStepWebs/simple-html-invoice-template</a>
+        </p>
     </div>
-    
   </body>
 </html>
 
@@ -132,13 +187,13 @@
   use Dompdf\Dompdf;
   // instantiate and use the dompdf class
   $dompdf = new Dompdf();
-  $dompdf->loadHtml( $html );
-  // Setup the paper size and orientation
-  $dompdf->setPaper('A4', 'portrait')
-  //Render the HTML as PDF
+  $dompdf->loadHtml($html);
+  // (Optional) Setup the paper size and orientation
+  $dompdf->setPaper('A4', 'portrait');
+  // Render the HTML as PDF
   $dompdf->render();
   // Output the generated PDF to Browser
-  $dompdf->stream( "receipt.pdf", array( 'Attachment' => false ) );
+  $dompdf->stream("receipt.pdf", array( 'Attachment' => false ) );
   exit(0);
 
 ?>
